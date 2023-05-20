c5804e2d95fe1062993f3518dbbebb436026b01d
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-05-20 15:59:09 +0530

    done

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+
 class loginController extends Controller
 {
     /**
@@ -12,43 +14,60 @@ class loginController extends Controller
      */
     public function __construct()
     {
-        
     }
 
-    public function adminLogin(){
+    public function adminLogin()
+    {
 
-         $data = json_decode(file_get_contents("php://input"));
-
-         $user = $data->userId;
-         $password= $data->password;
+        $data = json_decode(file_get_contents("php://input"));
+        $user = $data->userId;
+        $password = $data->password;
         // $payload = auth()->payload();
-         
-        try{
+        try {
 
-        
-        $logindata = DB::table('Admin')->where('UserId',$user)->get();
-        $result = DB::table('Admin')->where('UserId',$user)->get(['Id','name','UserId']);
-        if(count($logindata)>0 ){
-            if($password == $logindata[0]->Password){
-                $state = 200;
-                $msg = 'login successfully';
-                $result = $result;
-            }else{
-                $state = 400;
-                $msg = 'Password not match';
-                $result = [];
+
+            $logindata = DB::table('Admin')->where('UserId', $user)->get();
+            // $result = DB::table('Admin')->where('UserId',$user)->get(['Id','name','UserId']);
+            if (count($logindata) > 0) {
+                if ($password == $logindata[0]->Password) {
+
+                    $user_arr = array(
+                        "status" => true,
+                        "success" => true,
+                        "id" => $logindata[0]->UserId,
+                        "name" => $logindata[0]->name,
+                        "message" => "Login Successfully !",
+                    );
+                } else {
+                    $user_arr = array(
+                        "status" => false,
+                        "success" => false,
+                        "id" => '',
+                        "name" => '',
+                        "message" => "Password not match !",
+                    );
+                }
+            } else {
+
+                $user_arr = array(
+                    "status" => false,
+                    "success" => false,
+                    "id" => '',
+                    "name" => '',
+                    "message" => "User Id not match !",
+                );
             }
-        }else{
-            $state = 400;
-            $msg = 'Userid not match';
-            $result = $logindata;
-        } 
-    }catch(Exception  $e){
-            $state = 500;
-            $msg = 'Something Wrong Happened';
-            $result = $logindata;
-    }
+        } catch (Exception  $e) {
 
-        return array('status'=>$state , 'masssage' => $msg ,'result' => $result);
+            $user_arr = array(
+                "status" => false,
+                "success" => false,
+                "id" => '',
+                "name" => '',
+                "message" => "Something Wrong Happened!",
+            );
+        }
+
+        return json_encode($user_arr);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,30 @@
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Method: POST,GET');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type , Access-Control-Allow-Method , Authorization , X-Requested-with');
-header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Method: POST,GET,OPTIONS');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type , Access-Control-Allow-Method , Authorization , X-Requested-with');
+// header('Content-Type: application/json');
+
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+$allowed_domains = array(
+    'http://localhost:4200'
+
+);
+
+if (in_array(strtolower($http_origin), $allowed_domains)) {
+
+    header("Access-Control-Allow-Origin: $http_origin");
+    header('Access-Control-Allow-Method: POST,GET,OPTIONS');
+    header('Content-Type: application/json');
+    // header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type , Access-Control-Allow-Method , Authorization , X-Requested-with');
+}
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Authorization, Content-Type,Accept, Origin");
+    header("HTTP/1.1 200");
+    exit(0);
+}
 
 
 $router->post('adminLogin','loginController@adminLogin');
