34b16bcfeada3a950ddaf033b06d56369963c16a
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-06-05 09:43:02 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ $router->post("/state","addItemControler@state");
 $router->post("/fetch","dynamic_Crud_controller@fetch");
 $router->post("/save","dynamic_Crud_controller@save");
 $router->post("/insertData","dynamic_Crud_controller@insertData");
-$router->post("/userLogin","loginController@userLogin");
+$router->post("/auth","loginController@userLogin");
 
 
 
