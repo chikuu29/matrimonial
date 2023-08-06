4de791bd26f88b600eb17295f3f787e8c120d428
Gyana1995 <100307598+Gyana1995@users.noreply.github.com>
2023-08-06 12:31:13 +0530

    ok
    send

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -20,52 +20,52 @@ class filterController extends Controller
             );
         } else {
 
-           // try {
+            // try {
 
-               // $user_partnerpreference = DB::table('user_partnerpreference')->where('user_ID', $user_id)->get();
-                //$user_height = $user_partnerpreference[0]->user_height;
-                // dd($user_partnerpreference);
-                $user = DB::table('user_info')->where('user_ID', $user_id)->get('user_gender');
-                //dd($user);
-                $gender = $user[0]->user_gender == "male" ? 'female' : 'male';
-                //dd($gender);
-                $user_activities = DB::table('user_activities')->where('user_id', $user_id)->get('user_block_list');
-                if(count($user_activities) > 0){
-                    $user_block_list = $user_activities[0]->user_block_list;
+            // $user_partnerpreference = DB::table('user_partnerpreference')->where('user_ID', $user_id)->get();
+            //$user_height = $user_partnerpreference[0]->user_height;
+            // dd($user_partnerpreference);
+            $user = DB::table('user_info')->where('user_ID', $user_id)->get('user_gender');
+            //dd($user);
+            $gender = $user[0]->user_gender == "male" ? 'female' : 'male';
+            //dd($gender);
+            $user_activities = DB::table('user_activities')->where('user_id', $user_id)->get('user_block_list');
+            if (count($user_activities) > 0) {
+                $user_block_list = $user_activities[0]->user_block_list;
 
-                
+
                 $elements = explode(',', $user_block_list);
-                
+
                 // Enclose each element in double quotes
                 $quotedElements = array_map(function ($element) {
                     return '"' . $element . '"';
                 }, $elements);
-                
+
                 // Join the elements with commas
                 $outputString = implode(",", $quotedElements);
-                
-                }else{
-                    $outputString = '""';
-                }
-                
-               // echo $outputString;
 
-                
-                
+            } else {
+                $outputString = '""';
+            }
+
+            // echo $outputString;
 
 
-               // dd($user_block_list);
-                // dd("SELECT * FROM user_info  
-                // LEFT JOIN user_religion ON user_info.user_id = user_religion.user_ID 
-                // LEFT JOIN user_locations ON user_info.user_id = user_locations.user_ID 
-                // LEFT JOIN user_family ON user_info.user_id = user_family.user_ID 
-                // LEFT JOIN user_physical_details ON user_info.user_id = user_physical_details.user_ID
-                // LEFT JOIN user_about ON user_info.user_id = user_about.user_ID
-                // LEFT JOIN user_diet_hobbies ON user_info.user_id = user_diet_hobbies.user_ID
-                // LEFT JOIN user_education_occupations ON user_info.user_id = user_education_occupations.user_ID
-                // WHERE user_info.user_gender = '$gender'  AND user_info.user_status = 'Approved'  AND (user_info.user_id NOT IN ($outputString)) ;");
 
-                $alldata = DB::select("SELECT * FROM user_info  
+
+
+            // dd($user_block_list);
+            // dd("SELECT * FROM user_info  
+            // LEFT JOIN user_religion ON user_info.user_id = user_religion.user_ID 
+            // LEFT JOIN user_locations ON user_info.user_id = user_locations.user_ID 
+            // LEFT JOIN user_family ON user_info.user_id = user_family.user_ID 
+            // LEFT JOIN user_physical_details ON user_info.user_id = user_physical_details.user_ID
+            // LEFT JOIN user_about ON user_info.user_id = user_about.user_ID
+            // LEFT JOIN user_diet_hobbies ON user_info.user_id = user_diet_hobbies.user_ID
+            // LEFT JOIN user_education_occupations ON user_info.user_id = user_education_occupations.user_ID
+            // WHERE user_info.user_gender = '$gender'  AND user_info.user_status = 'Approved'  AND (user_info.user_id NOT IN ($outputString)) ;");
+
+            $alldata = DB::select("SELECT * FROM user_info  
             LEFT JOIN user_religion ON user_info.user_id = user_religion.user_ID 
             LEFT JOIN user_locations ON user_info.user_id = user_locations.user_ID 
             LEFT JOIN user_family ON user_info.user_id = user_family.user_ID 
@@ -74,30 +74,30 @@ class filterController extends Controller
             LEFT JOIN user_diet_hobbies ON user_info.user_id = user_diet_hobbies.user_ID
             LEFT JOIN user_education_occupations ON user_info.user_id = user_education_occupations.user_ID
             WHERE user_info.user_gender = '$gender'  AND user_info.user_status = 'Approved'   AND  user_info.user_id NOT IN ($outputString) ;");
-                // dd("SELECT * FROM user_info  
-                // LEFT JOIN user_religion ON user_info.user_id = user_religion.user_ID 
-                // LEFT JOIN user_locations ON user_info.user_id = user_locations.user_ID 
-                // LEFT JOIN user_family ON user_info.user_id = user_family.user_ID 
-                // LEFT JOIN user_physical_details ON user_info.user_id = user_physical_details.user_ID
-                // LEFT JOIN user_about ON user_info.user_id = user_about.user_ID
-                // LEFT JOIN user_diet_hobbies ON user_info.user_id = user_diet_hobbies.user_ID
-                // LEFT JOIN user_education_occupations ON user_info.user_id = user_education_occupations.user_ID
-                // WHERE user_info.user_gender = '$gender'  AND user_info.user_status = 'Approved'  AND user_info.user_id NOT IN ($user_block_list) ;");
+            // dd("SELECT * FROM user_info  
+            // LEFT JOIN user_religion ON user_info.user_id = user_religion.user_ID 
+            // LEFT JOIN user_locations ON user_info.user_id = user_locations.user_ID 
+            // LEFT JOIN user_family ON user_info.user_id = user_family.user_ID 
+            // LEFT JOIN user_physical_details ON user_info.user_id = user_physical_details.user_ID
+            // LEFT JOIN user_about ON user_info.user_id = user_about.user_ID
+            // LEFT JOIN user_diet_hobbies ON user_info.user_id = user_diet_hobbies.user_ID
+            // LEFT JOIN user_education_occupations ON user_info.user_id = user_education_occupations.user_ID
+            // WHERE user_info.user_gender = '$gender'  AND user_info.user_status = 'Approved'  AND user_info.user_id NOT IN ($user_block_list) ;");
 
-                if (count($alldata) > 0) {
-                    $user_arr = array(
-                        "status" => true,
-                        "success" => true,
-                        "data" => $alldata,
-                        "message" => count($alldata) . ' records Match'
-                    );
-                } else {
-                    $user_arr = array(
-                        "status" => false,
-                        "success" => false,
-                        "message" => "No Match Found",
-                    );
-                }
+            if (count($alldata) > 0) {
+                $user_arr = array(
+                    "status" => true,
+                    "success" => true,
+                    "data" => $alldata,
+                    "message" => count($alldata) . ' records Match'
+                );
+            } else {
+                $user_arr = array(
+                    "status" => false,
+                    "success" => false,
+                    "message" => "No Match Found",
+                );
+            }
             // } catch (Exception $e) {
             //     $user_arr = array(
             //         "status" => false,
@@ -113,7 +113,7 @@ class filterController extends Controller
     public function matchesforindivisual()
     {
         $data = json_decode(file_get_contents("php://input"));
-       
+
         $user_id = isset($data->user_id) ? $data->user_id : '';
         if ($user_id == '') {
             $user_arr = array(
@@ -123,25 +123,25 @@ class filterController extends Controller
             );
         } else {
 
-           // try {
+            // try {
 
-                $user_partnerpreference = DB::table('user_partnerpreference')->where('user_ID', $user_id)->get();
-                $user_height = $user_partnerpreference[0]->user_height;
-                $user_religion = $user_partnerpreference[0]->user_religion;
-                $user_country = $user_partnerpreference[0]->user_country;
-                $user_marital_status = $user_partnerpreference[0]->user_marital_status;
-                $user_city = $user_partnerpreference[0]->user_city;
-                $user_employed_In = $user_partnerpreference[0]->user_employed_In;
-                $user_occupation = $user_partnerpreference[0]->user_occupation;
-                $user_mother_toungh = $user_partnerpreference[0]->user_mother_toungh;
-                $user_max_anual_income = $user_partnerpreference[0]->user_max_anual_income;
-                $user_min_anual_income = $user_partnerpreference[0]->user_min_anual_income;
-                 
-                $user = DB::table('user_info')->where('user_ID', $user_id)->get('user_gender');
-                
-                $gender = $user[0]->user_gender == "male" ? 'female' : 'male';
-                
-                $alldata = DB::select("SELECT * FROM user_info  
+            $user_partnerpreference = DB::table('user_partnerpreference')->where('user_ID', $user_id)->get();
+            $user_height = $user_partnerpreference[0]->user_height;
+            $user_religion = $user_partnerpreference[0]->user_religion;
+            $user_country = $user_partnerpreference[0]->user_country;
+            $user_marital_status = $user_partnerpreference[0]->user_marital_status;
+            $user_city = $user_partnerpreference[0]->user_city;
+            $user_employed_In = $user_partnerpreference[0]->user_employed_In;
+            $user_occupation = $user_partnerpreference[0]->user_occupation;
+            $user_mother_toungh = $user_partnerpreference[0]->user_mother_toungh;
+            $user_max_anual_income = $user_partnerpreference[0]->user_max_anual_income;
+            $user_min_anual_income = $user_partnerpreference[0]->user_min_anual_income;
+
+            $user = DB::table('user_info')->where('user_ID', $user_id)->get('user_gender');
+
+            $gender = $user[0]->user_gender == "male" ? 'female' : 'male';
+
+            $alldata = DB::select("SELECT * FROM user_info  
             LEFT JOIN user_religion ON user_info.user_id = user_religion.user_ID 
             LEFT JOIN user_locations ON user_info.user_id = user_locations.user_ID 
             LEFT JOIN user_family ON user_info.user_id = user_family.user_ID 
@@ -160,22 +160,22 @@ class filterController extends Controller
             ( user_info.user_gender = '$gender' AND user_info.user_status = 'Approved' )
             
             ;");
-            
 
-                if (count($alldata) > 0) {
-                    $user_arr = array(
-                        "status" => true,
-                        "success" => true,
-                        "data" => $alldata,
-                        "message" => count($alldata) . ' records Match'
-                    );
-                } else {
-                    $user_arr = array(
-                        "status" => false,
-                        "success" => false,
-                        "message" => "No Match Found",
-                    );
-                }
+
+            if (count($alldata) > 0) {
+                $user_arr = array(
+                    "status" => true,
+                    "success" => true,
+                    "data" => $alldata,
+                    "message" => count($alldata) . ' records Match'
+                );
+            } else {
+                $user_arr = array(
+                    "status" => false,
+                    "success" => false,
+                    "message" => "No Match Found",
+                );
+            }
             // } catch (Exception $e) {
             //     $user_arr = array(
             //         "status" => false,
@@ -187,7 +187,83 @@ class filterController extends Controller
         }
         return json_encode($user_arr);
     }
-    
-    
+    public function matchPersent()
+    {
+        $data = json_decode(file_get_contents("php://input"));
+        // dd($data);
+        $loginuserid = isset($data->loginuserid) ? $data->loginuserid : '';
+        $preferenceuserid = isset($data->preferenceuserid) ? $data->preferenceuserid : '';
+
+        try {
+
+            $user_partnerpreference = DB::table('user_partnerpreference')->where('user_ID', $loginuserid)->get();
+            // dd($user_partnerpreference);
+            $user_height = $user_partnerpreference[0]->user_height;
+            $user_religion = $user_partnerpreference[0]->user_religion;
+            $user_country = $user_partnerpreference[0]->user_country;
+            $user_marital_status = $user_partnerpreference[0]->user_marital_status;
+            $user_city = $user_partnerpreference[0]->user_city;
+            $user_employed_In = $user_partnerpreference[0]->user_employed_In;
+            $user_occupation = $user_partnerpreference[0]->user_occupation;
+            $user_mother_toungh = $user_partnerpreference[0]->user_mother_toungh;
+            $user_max_anual_income = $user_partnerpreference[0]->user_max_anual_income;
+            $user_max_anual_income = $user_partnerpreference[0]->user_min_anual_income;
+
+            $income = array($user_max_anual_income, $user_max_anual_income);
+
+            $user_physical_details = DB::table('user_physical_details')->where('user_ID', $preferenceuserid)->where('user_height', $user_height)->exists();
+            $user_religion = DB::table('user_religion')->where('user_ID', $preferenceuserid)->where('user_caste', $user_religion)->exists();
+            $user_education_occupations = DB::table('user_education_occupations')->where('user_ID', $preferenceuserid)->where('user_employed_In', $user_employed_In)->exists();
+            $user_education_occupations1 = DB::table('user_education_occupations')->where('user_ID', $preferenceuserid)->where('user_occupation', $user_occupation)->exists();
+            $user_info = DB::table('user_info')->where('user_ID', $preferenceuserid)->where('user_mother_toungh', $user_mother_toungh)->exists();
+            $user_education_occupations2 = DB::table('user_education_occupations')->where('user_ID', $preferenceuserid)->whereBetween('user_anual_income', $income)->toSql();
+            // dd($user_education_occupations2);
+            if ($user_physical_details) {
+                $one = 1;
+            } else {
+                $one = 0;
+            }
+            if ($user_religion) {
+                $two = 1;
+            } else {
+                $two = 0;
+            }
+            if ($user_education_occupations) {
+                $three = 1;
+            } else {
+                $three = 0;
+            }
+            if ($user_education_occupations1) {
+                $foure = 1;
+            } else {
+                $foure = 0;
+            }
+            if ($user_info) {
+                $five = 1;
+            } else {
+                $five = 0;
+            }
+            if ($user_education_occupations2) {
+                $six = 1;
+            } else {
+                $six = 0;
+            }
+            $persent = ($one + $two + $three + $foure + $five + $six) / 6 * 100;
+            $user_arr = array(
+                "status" => true,
+                "success" => true,
+                "message" => round($persent),
+            );
+        }catch(Exception $e){
+            $user_arr = array(
+                "status" => false,
+                "success" => false,
+                "message" => [],
+            );
+        }
+        return json_encode($user_arr);
+    }
+
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ $router->post("/country","addItemControler@country");
 $router->post("/state","addItemControler@state");
 $router->post("/fetch","dynamic_Crud_controller@fetch");
 $router->post("/save","dynamic_Crud_controller@save");
+$router->post("/delete","dynamic_Crud_controller@delete");
 $router->post("/update","dynamic_Crud_controller@update");
 $router->post("/insertData","dynamic_Crud_controller@insertData");
 $router->post("/auth","loginController@userLogin");
@@ -45,6 +46,8 @@ $router->post("/profileValidation","userController@profileValidation");
 $router->post("/getDataFormQuery","dynaController@dynaQuay");
 $router->post("/matches","filterController@matches");
 $router->post("/matchesforindivisual","filterController@matchesforindivisual");
+$router->post("/matchPersent","filterController@matchPersent");
+
 
 
 
