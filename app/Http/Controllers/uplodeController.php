9ed17499d774c8d69e47283c540effe817032c38
Gyana1995 <100307598+Gyana1995@users.noreply.github.com>
2023-11-04 03:49:03 +0530

    ok
    gyana

--- app/Http/Controllers/uplodeController.php
+++ app/Http/Controllers/uplodeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class uplodeController extends Controller
 {
     public function logoUplode(Request $res)
@@ -14,21 +15,21 @@ class uplodeController extends Controller
         $image = explode(';base64,', $input['image']);
         $image_base64 = base64_decode($image[1]);
         $extention = explode('/', $image[0]);
-        $path = storage_path().'/logo_image/';
+        $path = storage_path() . '/logo_image/';
         $uniqid = uniqid();
         $file = $path . $uniqid . '.' . $extention[1];
-        if(file_put_contents($file, $image_base64)){
-           $data = DB::table('logo_table')->insert([
+        if (file_put_contents($file, $image_base64)) {
+            $data = DB::table('logo_table')->insert([
                 'image' => $uniqid . '.' . $extention[1],
                 'created_At' => $date
-           ]);
-           if($data){
-            $user_arr = array(
-                "success" => true,
-                "message" => "File Uploaded Successfully",
-            );
-           }
-        }else{
+            ]);
+            if ($data) {
+                $user_arr = array(
+                    "success" => true,
+                    "message" => "File Uploaded Successfully",
+                );
+            }
+        } else {
             $user_arr = array(
                 "success" => false,
                 "message" => "Error",
@@ -41,30 +42,30 @@ class uplodeController extends Controller
     {
 
         $input = $res->all();
-       // return $input;
+        // return $input;
         $date = $input['date'];
         $index = $input['index'];
         $image = explode(';base64,', $input['image']);
         $image_base64 = base64_decode($image[1]);
         $extention = explode('/', $image[0]);
-        $path = storage_path().'/banner/';
+        $path = storage_path() . '/banner/';
         $uniqid = uniqid();
         $file = $path . $uniqid . '.' . $extention[1];
 
 
-        if(file_put_contents($file, $image_base64)){
-           $data = DB::table('banner_image')->insert([
+        if (file_put_contents($file, $image_base64)) {
+            $data = DB::table('banner_image')->insert([
                 'image' => $uniqid . '.' . $extention[1],
                 'created_At' => $date,
                 'index' => $index
-           ]);
-           if($data){
-            $user_arr = array(
-                "success" => true,
-                "message" => "File Uploaded Successfully",
-            );
-           }
-        }else{
+            ]);
+            if ($data) {
+                $user_arr = array(
+                    "success" => true,
+                    "message" => "File Uploaded Successfully",
+                );
+            }
+        } else {
             $user_arr = array(
                 "success" => false,
                 "message" => "Error",
@@ -76,29 +77,29 @@ class uplodeController extends Controller
     {
 
         $input = $res->all();
-      // return $input;
+        // return $input;
         $date = $input['date'];
-       
+
         $image = explode(';base64,', $input['image']);
         $image_base64 = base64_decode($image[1]);
         $extention = explode('/', $image[0]);
-        $path = storage_path().'/watermark/';
+        $path = storage_path() . '/watermark/';
         $uniqid = uniqid();
         $file = $path . $uniqid . '.' . $extention[1];
 
 
-        if(file_put_contents($file, $image_base64)){
-           $data = DB::table('watermark')->insert([
+        if (file_put_contents($file, $image_base64)) {
+            $data = DB::table('watermark')->insert([
                 'image' => $uniqid . '.' . $extention[1],
                 'created_At' => $date,
-           ]);
-           if($data){
-            $user_arr = array(
-                "success" => true,
-                "message" => "File Uploaded Successfully",
-            );
-           }
-        }else{
+            ]);
+            if ($data) {
+                $user_arr = array(
+                    "success" => true,
+                    "message" => "File Uploaded Successfully",
+                );
+            }
+        } else {
             $user_arr = array(
                 "success" => false,
                 "message" => "Error",
@@ -110,29 +111,34 @@ class uplodeController extends Controller
     {
 
         $input = $res->all();
-      // return $input;
+        //return $input;
         $date = $input['date'];
-       
+        $name = $input['name'];
+        $phoneno = $input['phoneno'];
+        $upi = $input['upi'];
         $image = explode(';base64,', $input['image']);
         $image_base64 = base64_decode($image[1]);
         $extention = explode('/', $image[0]);
-        $path = storage_path().'/barcode/';
+        $path = storage_path() . '/barcode/';
         $uniqid = uniqid();
         $file = $path . $uniqid . '.' . $extention[1];
 
 
-        if(file_put_contents($file, $image_base64)){
-           $data = DB::table('barCode')->insert([
+        if (file_put_contents($file, $image_base64)) {
+            $data = DB::table('barCode')->insert([
                 'image' => $uniqid . '.' . $extention[1],
                 'created_At' => $date,
-           ]);
-           if($data){
-            $user_arr = array(
-                "success" => true,
-                "message" => "File Uploaded Successfully",
-            );
-           }
-        }else{
+                'name' => $name,
+                'phoneno' => $phoneno,
+                'upi' =>$upi
+            ]);
+            if ($data) {
+                $user_arr = array(
+                    "success" => true,
+                    "message" => "File Uploaded Successfully",
+                );
+            }
+        } else {
             $user_arr = array(
                 "success" => false,
                 "message" => "Error",
